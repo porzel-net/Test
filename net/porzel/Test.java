a62a343e449c965f50a870984add7ef3137d733d
juliusporzel <julius.porzel@web.de>
2023-04-12 13:15:53 +0200

    - Second a.2 commit

--- net/porzel/Test.java
+++ net/porzel/Test.java
@@ -1,7 +1,7 @@
 package net.porzel;
 
 public class Test {
-    private static final String version = "a.1";
+    private static final String version = "a.2";
 
     public static void main(String[] args) {
         System.out.println(version);
